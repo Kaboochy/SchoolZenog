c54a65b69262bcc3a7b1325f14b697fae436ce72
Jacob-Molina <100715144+Jacob-Molina@users.noreply.github.com>
2022-03-22 12:08:29 -0500

    Add files via upload

---
+++ SchoolZenog/SchoolZenog/SchoolZenog/Animations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SchoolZenog
+{
+    class Animations
+    {
+        public List<Animation> enter;
+        public List<Animation> idle;
+        public List<Animation> walk;
+        public List<Animation> run;
+        public List<Animation> jump;
+        public List<Animation> fallingL;
+        public List<Animation> fallingH;
+        public List<Animation> fallingA;
+        public List<Animation> fallingS;
+        public List<Animation> recover;
+        public List<Animation> attack11;
+        public List<Animation> attack12;
+        public List<Animation> attack13;
+        public List<Animation> attack21;
+        public List<Animation> attack22;
+        public List<Animation> attack23;
+        public List<Animation> attackA1;
+        public List<Animation> attackA2;
+        public List<Animation> attackA3;
+        public List<Animation> blockS;
+        public List<Animation> blockA;
+        public List<Animation> ult;
+        public List<Animation> death;
+    }
+}
