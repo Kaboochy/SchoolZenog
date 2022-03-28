5ef17c0d8449932a265945e11da0f6877a0c67ba
Jacob Molina <jacob.a.molina1@gmail.com>
2022-03-28 12:05:46 -0500

    Update Zy.cs

--- SchoolZenog/SchoolZenog/SchoolZenog/Zy.cs
+++ SchoolZenog/SchoolZenog/SchoolZenog/Zy.cs
@@ -1,11 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace SchoolZenog
 {
     class Zy
     {
+        KeyboardState kb, oldKb;
+        MouseState mouse, oldMouse;
+        public Zy(Texture2D tex)
+        {
+
+        }
     }
 }
