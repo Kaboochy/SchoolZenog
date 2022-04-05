cea560fd3c01ebdc220d3a05aa25250ad3448f37
Jacob Molina <jacob.a.molina1@gmail.com>
2022-04-05 12:14:48 -0500

    Update Zy.cs

--- SchoolZenog/SchoolZenog/SchoolZenog/Zy.cs
+++ SchoolZenog/SchoolZenog/SchoolZenog/Zy.cs
@@ -20,95 +20,95 @@ namespace SchoolZenog
         {
             for (int i = 0; i < 5; i++)
             {
-                anime.enter.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_enter.txt"));
+                anime.enter.Add(new Animation(tex, i, 1, 150, @"Content/Zy_enter.txt"));
             }
             for (int i = 0; i < 8; i++)
             {
-                anime.idle.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_idle.txt"));
+                anime.idle.Add(new Animation(tex, i, 1, 150, @"Content/Zy_idle.txt"));
             }
             for (int i = 0; i < 6; i++)
             {
-                anime.walk.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_walk.txt"));
+                anime.walk.Add(new Animation(tex, i, 1, 150, @"Content/Zy_walk.txt"));
             }
             for (int i = 0; i < 6; i++)
             {
-                anime.run.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_run.txt"));
+                anime.run.Add(new Animation(tex, i, 1, 150, @"Content/Zy_run.txt"));
             }
             for (int i = 0; i < 3; i++)
             {
-                anime.jump.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_jump.txt"));
+                anime.jump.Add(new Animation(tex, i, 1, 150, @"Content/Zy_jump.txt"));
             }
             for (int i = 0; i < 2; i++)
             {
-                anime.fallingL.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_fallingL.txt"));
+                anime.fallingL.Add(new Animation(tex, i, 1, 150, @"Content/Zy_fallingL.txt"));
             }
             for (int i = 0; i < 2; i++)
             {
-                anime.fallingH.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_fallingH.txt"));
+                anime.fallingH.Add(new Animation(tex, i, 1, 150, @"Content/Zy_fallingH.txt"));
             }
             for (int i = 0; i < 3; i++)
             {
-                anime.fallingA.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_fallingA.txt"));
+                anime.fallingA.Add(new Animation(tex, i, 1, 150, @"Content/Zy_fallingA.txt"));
             }
             for (int i = 0; i < 2; i++)
             {
-                anime.fallingS.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_fallingS.txt"));
+                anime.fallingS.Add(new Animation(tex, i, 1, 150, @"Content/Zy_fallingS.txt"));
             }
             for (int i = 0; i < 7; i++)
             {
-                anime.recover.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_recover.txt"));
+                anime.recover.Add(new Animation(tex, i, 1, 150, @"Content/Zy_recover.txt"));
             }
             for (int i = 0; i < 3; i++)
             {
-                anime.attack11.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack11.txt"));
+                anime.attack11.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack11.txt"));
             }
             for (int i = 0; i < 4; i++)
             {
-                anime.attack12.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack12.txt"));
+                anime.attack12.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack12.txt"));
             }
             for (int i = 0; i < 4; i++)
             {
-                anime.attack13.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack13.txt"));
+                anime.attack13.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack13.txt"));
             }
             for (int i = 0; i < 5; i++)
             {
-                anime.attack21.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack21.txt"));
+                anime.attack21.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack21.txt"));
             }
             for (int i = 0; i < 4; i++)
             {
-                anime.attack22.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack22.txt"));
+                anime.attack22.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack22.txt"));
             }
             for (int i = 0; i < 6; i++)
             {
-                anime.attack23.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attack23.txt"));
+                anime.attack23.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attack23.txt"));
             }
             for (int i = 0; i < 5; i++)
             {
-                anime.attackA1.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attackA1.txt"));
+                anime.attackA1.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attackA1.txt"));
             }
             for (int i = 0; i < 6; i++)
             {
-                anime.attackA2.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attackA2.txt"));
+                anime.attackA2.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attackA2.txt"));
             }
             for (int i = 0; i < 3; i++)
             {
-                anime.attackA3.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_attackA3.txt"));
+                anime.attackA3.Add(new Animation(tex, i, 1, 150, @"Content/Zy_attackA3.txt"));
             }
             for (int i = 0; i < 1; i++)
             {
-                anime.blockS.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_blockS.txt"));
+                anime.blockS.Add(new Animation(tex, i, 1, 150, @"Content/Zy_blockS.txt"));
             }
             for (int i = 0; i < 1; i++)
             {
-                anime.blockA.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_blockA.txt"));
+                anime.blockA.Add(new Animation(tex, i, 1, 150, @"Content/Zy_blockA.txt"));
             }
             for (int i = 0; i < 8; i++)
             {
-                anime.ult.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_ult.txt"));
+                anime.ult.Add(new Animation(tex, i, 1, 150, @"Content/Zy_ult.txt"));
             }
             for (int i = 0; i < 8; i++)
             {
-                anime.death.Add(new Animation(tex, i, 1, @"Content/Hitboxes/Zy/Zy_death.txt"));
+                anime.death.Add(new Animation(tex, i, 1, 150, @"Content/Zy_death.txt"));
             }
         }
         public void Update(KeyboardState Kb, MouseState Mouse)
@@ -116,8 +116,39 @@ namespace SchoolZenog
             kb = Kb;
             mouse = Mouse;
 
+            //idle
+            if (stop == 0)
+            {
+                currentAnime = Animated.idle;
+            }
+            //movement
+            if (kb.IsKeyDown(Keys.D) && !kb.IsKeyDown(Keys.A) && stop == 0)
+            {
+                currentAnime = Animated.walk;
+                right = true;
+            }
+            if (kb.IsKeyDown(Keys.A) && !kb.IsKeyDown(Keys.D) && stop == 0)
+            {
+                currentAnime = Animated.walk;
+                right = false;
+            }
+            if (currentAnime == Animated.walk && kb.IsKeyDown(Keys.LeftShift))
+            {
+                currentAnime = Animated.run;
+            }
+            //attack
+            if (mouse.LeftButton == ButtonState.Pressed && oldMouse.LeftButton == ButtonState.Released && stop == 0)
+            {
+                currentAnime = Animated.attack21;
+                stop = 1;
+            }
+            //frame update
+            up();
+            // hitbox logic
+
             oldKb = kb;
             oldMouse = mouse;
+            lastAnime = currentAnime;
         }
     }
 }
